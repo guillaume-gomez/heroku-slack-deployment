febbed1a0d0b77dcffbd1e378f66ef2dcf66485c
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-10-21 00:31:38 +0200

    add slack auth + dotenv

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
+require 'dotenv/load'
 require 'sinatra'
+
+require_relative 'app/slack_authorizer'
+use SlackAuthorizer
+
 
 post '/slack/command' do
   "Ok, this is a test message for a bot. Sorry to bother you ! :)"
