93465d0381576a3e070a0963474a22e5e8412634
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-10-23 00:07:17 +0200

    move inclusion file in app to config

--- app.rb
+++ app.rb
@@ -3,17 +3,23 @@ require 'byebug'
 require 'sinatra'
 require 'json'
 
-require_relative 'app/heroku_helper'
-require_relative 'modules/slack_helper'
+#use SlackAuthorizer
 
-# require_relative 'app/slack_authorizer'
-# use SlackAuthorizer
-# use it when slack/command will be enable
-post '/slack/command' do
-  "Ok, this is a test message for a bot. Sorry to bother you ! :)"
+require 'sinatra/base'
+
+class HerokuSlack < Sinatra::Base
+
+  helpers Sinatra::HerokuSlack::SlackHelper
+  helpers Sinatra::HerokuSlack::HerokuHelper
+
+  post '/slack/command' do
+    "Ok, this is a test message for a bot. Sorry to bother you ! :)"
+  end
+
+  post '/heroku/hooks' do
+    body = JSON.parse(request.body.read)
+    post_deployment_message("#general", filtered_params(body))
+  end
 end
 
-post '/heroku/hooks' do
-  body = JSON.parse(request.body.read)
-  post_deployment_message("#general", filtered_params(body))
-end
+
--- config.ru
+++ config.ru
@@ -1,2 +1,9 @@
+require_relative 'helpers/heroku_helper'
+require_relative 'helpers/slack_helper'
+
+require_relative 'app/slack_authorizer'
+
+#require_relative 'models/heroku_webhook'
+
 require './app'
-run Sinatra::Application
+run HerokuSlack
